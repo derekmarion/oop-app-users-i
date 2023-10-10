76d3f5737a8b0dbf7bcb55b163fec7ac0f156ef6
Derek Marion <derek.j.marion@gmail.com>
2023-10-09 17:04:49 -0700

    Create a User class, generate some users, and output their data

--- User.py
+++ User.py
@@ -1,1 +1,14 @@
-# your User class goes here
+class User:
+    def __init__(self, name, email, license) -> None:
+        self.name = name
+        self.email = email
+        self.license = license
+
+    def __str__(self) -> str:
+        return f"Username: {self.name}\nEmail: {self.email}\nLicense #: {self.license}"
+
+user1 = User("Jimmy", "jimmy@gimmy.com", "as;lkdjfao92wi3")
+user2 = User("Johnny", "johnny@johnny.com", "a;lkm219o2wre")
+
+print(user1)
+print(user2)
